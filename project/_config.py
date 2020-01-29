ef8e4347bee65027c22e2adffee8dc743c977e7c
Haen <>
2020-01-29 18:16:55 +0800

    up to page 146

--- project/_config.py
+++ project/_config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = '\xe0\xcaaP\xc2\xd4\xb8\xb5\xcai\xb4\xb70L\xcf-[ae\xf0<\xe0r\xd8'
 
 # define the full path for the database
